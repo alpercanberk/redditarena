c0a16102b13e0aea2416679ccea740bcd2b92a39
nolcay <olcay.oransoy@gmail.com>
2018-06-20 11:43:50 +0300

    Created the function that requests the bosses from the Reddit API

---
+++ api/reddit_api_accessor.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+// getBossesAndUpdateDatabase requests the hot 8 bossfights from the Reddit API.
+func getBosses() (json string) {
+	// GET requests the hot 8 bossfights from the Reddit API
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://reddit.com/r/BossFights/hot.json?limit=8&show=all", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	req.Header.Add("User-Agent", "osx:redditarena:v1 (by /u/GrEnAdIfY)")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	byteSlice, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	json = string(byteSlice)
+	return
+}
